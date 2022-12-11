6f100a921187543896d730cd2ae88d1b5082a8ce
Vi Đăng Khoa <75578740+khoavd1102@users.noreply.github.com>
2022-12-11 16:06:57 +0700

    1211

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -14,6 +14,7 @@ public class Mover : MonoBehaviour
         {
             MoveToCursor();
         }
+        UpdateAnimator();
     }
 
     private void MoveToCursor()
@@ -25,5 +26,13 @@ public class Mover : MonoBehaviour
         {
             GetComponent<NavMeshAgent>().destination = hit.point;
         }
+    }
+
+    private void UpdateAnimator()
+    {
+        Vector3 velocity = GetComponent<NavMeshAgent>().velocity;
+        Vector3 localVelocity = transform.InverseTransformDirection(velocity);
+        float speed = localVelocity.z;
+        GetComponent<Animator>().SetFloat("forwardSpeed", speed);
     }
 }
