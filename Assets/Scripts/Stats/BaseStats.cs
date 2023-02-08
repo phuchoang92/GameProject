c86d31c7b02ae1fa2b55b707d6fd1edf4e9ffe57
O1kura <viet6112001@gmail.com>
2023-02-09 05:27:23 +0700

    Update leveling up
    Leveling up and no more moving while attacking

--- Assets/Scripts/Stats/BaseStats.cs
+++ Assets/Scripts/Stats/BaseStats.cs
@@ -16,6 +16,21 @@ namespace Game.Stats
             return progression.GetStat(stats, characterClass, startingLevel); 
         }
 
+        public int GetLevel()
+        {
+            float currentEXP = GetComponent<Experience>().GetEXP();
+            int penultimateLevel = progression.GetLevels(Stats.ExperienceToLevelUp, characterClass);
+            for (int level = 1; level <= penultimateLevel; level++)
+            {
+                float EXPToLevelUp = progression.GetStat(Stats.ExperienceToLevelUp, characterClass, level);
+                if(EXPToLevelUp > currentEXP)
+                {
+                    return level;
+                }
+            }
+
+            return penultimateLevel + 1;
+        }
         public bool ProgressionCheck()
         { 
             return progression != null; 
